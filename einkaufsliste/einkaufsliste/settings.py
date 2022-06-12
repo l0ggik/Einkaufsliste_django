d616344b1c858bb4850eb51278de28ee98ef4f49
Arne Mross <baronarne@yahoo.de>
2022-06-12 14:08:57 +0200

    Changed settings.py

--- einkaufsliste/einkaufsliste/settings.py
+++ einkaufsliste/einkaufsliste/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-n3vz1d0w&we9t+1t)#l7#pn)xxq0luzx+3!umjcoha)c%*h=y1'
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
