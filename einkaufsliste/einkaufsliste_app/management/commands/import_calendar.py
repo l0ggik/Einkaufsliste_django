84fcdf095f589cf6bba7fe3dded0e002e28085cb
Arne Mross <baronarne@yahoo.de>
2023-07-10 20:57:51 +0200

    Updated calendar import script

--- einkaufsliste/einkaufsliste_app/management/commands/import_calendar.py
+++ einkaufsliste/einkaufsliste_app/management/commands/import_calendar.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from einkaufsliste_app.models import WasteEvent
 from ics import Calendar
+import requests
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -13,8 +14,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if options['import']:
-            with open("./Abfuhrtermine.ics") as file:
-                c = Calendar(file.read())
+            response = requests.get('https://www.awsh.de/api_v2/collection_dates/1/ort/561/strasse/220/hausnummern/0/abfallarten/R02-B02-D02-P04/kalender.ics')
+            c = Calendar(response.text)
             events = list(c.timeline)
             for event in events:
                 waste_event = WasteEvent(name=event.name, event_date=event.begin.date())
